ee3c6cdb30ebf82ec94a9c64ba3e3e674ae47ec2
abhijitrao <abhijitrao33@gmail.com>
2022-01-24 01:34:32 +0530

    database

--- database/droid_apps.sql
+++ database/droid_apps.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 20, 2022 at 02:57 PM
+-- Generation Time: Jan 23, 2022 at 09:03 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.3.27
 
@@ -20,6 +20,31 @@ SET time_zone = "+00:00";
 --
 -- Database: `droid_apps`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `table_account`
+--
+
+CREATE TABLE `table_account` (
+  `id` int(100) NOT NULL,
+  `name` varchar(500) NOT NULL,
+  `role` int(100) NOT NULL DEFAULT 0,
+  `assign_app` int(100) NOT NULL DEFAULT 0,
+  `user_id` varchar(500) NOT NULL,
+  `password` varchar(500) NOT NULL,
+  `active` int(100) NOT NULL DEFAULT 1,
+  `validity` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `table_account`
+--
+
+INSERT INTO `table_account` (`id`, `name`, `role`, `assign_app`, `user_id`, `password`, `active`, `validity`) VALUES
+(1, 'Admin', 1, 1, 'admin', '123', 1, '2024-01-31 19:15:44'),
+(2, 'Manager', 0, 2, 'manager', 'manager@123', 1, '2022-05-17 19:16:26');
 
 -- --------------------------------------------------------
 
@@ -59,7 +84,7 @@ CREATE TABLE `table_category` (
   `visibility` int(100) NOT NULL DEFAULT 1,
   `json_data` varchar(5000) DEFAULT NULL,
   `other_property` varchar(1000) DEFAULT NULL,
-  `updated_at` varchar(100) NOT NULL
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -67,9 +92,10 @@ CREATE TABLE `table_category` (
 --
 
 INSERT INTO `table_category` (`pkg_id`, `cat_id`, `sub_cat_id`, `cat_name`, `cat_type`, `image`, `order_id`, `visibility`, `json_data`, `other_property`, `updated_at`) VALUES
-('com.sample.live', 13, 5, 'Category Live Exam', 0, NULL, 0, 1, NULL, NULL, '2022-01-20 12:58:20'),
-('com.sample.live', 14, 5, 'Category Live Exam 2', 0, NULL, 0, 1, NULL, NULL, '2022-01-20 12:58:20'),
-('com.bizwiz.global', 15, 5, 'BizWiz Category 1', 0, NULL, 0, 1, NULL, NULL, '2022-01-20 12:58:20');
+('com.sample.live', 13, 5, 'Category Live Exam', 0, NULL, 0, 1, NULL, NULL, '2022-01-20 07:28:20'),
+('com.sample.live', 14, 5, 'Category Live Exam 2', 0, NULL, 0, 1, NULL, NULL, '2022-01-20 07:28:20'),
+('com.bizwiz.global', 15, 5, 'BizWiz Category 1', 0, NULL, 0, 1, NULL, NULL, '2022-01-20 07:28:20'),
+('com.app', 97, 0, 'eqweqwe', 0, NULL, 0, 0, '', '', '2022-01-23 20:00:03');
 
 -- --------------------------------------------------------
 
@@ -100,33 +126,15 @@ INSERT INTO `table_content` (`pkg_id`, `id`, `cat_id`, `sub_cat_id`, `title`, `d
 ('com.sample.live', 3, 13, 5, 'Live exam scheduled on 25th ', NULL, NULL, NULL, 1, NULL, NULL, '2022-01-20 12:59:38'),
 ('com.bizwiz.global', 4, 13, 5, 'BizWiz video striming soon.', NULL, NULL, NULL, 1, NULL, NULL, '2022-01-20 12:59:38');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `table_role`
---
-
-CREATE TABLE `table_role` (
-  `id` int(100) NOT NULL,
-  `name` varchar(500) NOT NULL,
-  `role` int(100) NOT NULL DEFAULT 0,
-  `user_id` varchar(500) NOT NULL,
-  `password` varchar(500) NOT NULL,
-  `active` int(100) NOT NULL DEFAULT 1,
-  `validity` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `table_role`
---
-
-INSERT INTO `table_role` (`id`, `name`, `role`, `user_id`, `password`, `active`, `validity`) VALUES
-(1, 'Admin', 1, 'admin', 'asxcv@123', 1, '2024-01-31 19:15:44'),
-(2, 'Manager', 0, 'manager', 'manager@123', 1, '2022-05-17 19:16:26');
-
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `table_account`
+--
+ALTER TABLE `table_account`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `table_app`
@@ -149,14 +157,14 @@ ALTER TABLE `table_content`
   ADD UNIQUE KEY `id` (`id`);
 
 --
--- Indexes for table `table_role`
---
-ALTER TABLE `table_role`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `table_account`
+--
+ALTER TABLE `table_account`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `table_app`
@@ -168,19 +176,13 @@ ALTER TABLE `table_app`
 -- AUTO_INCREMENT for table `table_category`
 --
 ALTER TABLE `table_category`
-  MODIFY `cat_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `cat_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=98;
 
 --
 -- AUTO_INCREMENT for table `table_content`
 --
 ALTER TABLE `table_content`
   MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `table_role`
---
-ALTER TABLE `table_role`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
